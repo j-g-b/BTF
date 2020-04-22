4cd9a30309fb5ab8f6a34ebdb74e5857db65ab43
Jordan Bryan <jb@Jordans-MacBook-Pro-4.local>
2020-04-22 10:23:39 -0400

    adding scripts to generate posterior samples and run simulation studies

---
+++ PrepareInputs.R
@@ -0,0 +1,9 @@
+require(magrittr)
+require(tidyverse)
+#
+cl_info <- readr::read_csv("data/sample_info.csv")
+#
+BTF::prepare_ge_rds("data/CCLE_expression.csv", "data/rds_obj/depmap_gene_expression.rds")
+BTF::prepare_crispr_rds("data/Achilles_gene_effect.csv", "data/rds_obj/depmap_crispr.rds")
+BTF::prepare_rnai_rds("data/D2_combined_gene_dep_scores.csv", "data/rds_obj/depmap_rnai.rds", cl_info)
+BTF::prepare_mut_rds("data/CCLE_mutations.csv", "data/rds_obj/depmap_mut.rds")
